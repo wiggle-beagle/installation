4dc95c2cd5c86f5a96abb6c44c08cd1c54b17196
olakulikova <helyakulikova@gmail.com>
2021-01-19 01:25:01 +0300

    files changed

--- src/Main.java
+++ src/Main.java
@@ -7,67 +7,47 @@ public class Main {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder();
         File file = new File("/Users/olakulikova/Games");
-
-        File src = new File("/Users/olakulikova/Games/src");
-        src.mkdir();
-        str.append("Каталог ").append(src.getName()).append(" создан\n");
-
-        File res = new File("/Users/olakulikova/Games/res");
-        res.mkdir();
-        str.append("Каталог ").append(res.getName()).append(" создан\n");
-
-        File savegames = new File("/Users/olakulikova/Games/savegames");
-        savegames.mkdir();
-        str.append("Каталог ").append(savegames.getName()).append(" создан\n");
-
-        File temp = new File("/Users/olakulikova/Games/temp");
-        temp.mkdir();
-        str.append("Каталог ").append(temp.getName()).append(" создан\n");
-
-        File main = new File("/Users/olakulikova/Games/src/main");
-        main.mkdir();
-        str.append("Каталог ").append(main.getName()).append(" создан\n");
-
-        File test = new File("/Users/olakulikova/Games/src/test");
-        test.mkdir();
-        str.append("Каталог ").append(test.getName()).append(" создан\n");
-
-        File mainJava = new File("/Users/olakulikova/Games/src/main//Main.java");
-        try {
-            mainJava.createNewFile();
-            str.append("Файл ").append(mainJava.getName()).append(" создан\n");
-        } catch (IOException ex) {
-        }
-
-        File utilsJava = new File("/Users/olakulikova/Games/src/main//Utils.java");
-        try {
-            utilsJava.createNewFile();
-            str.append("Файл ").append(utilsJava.getName()).append(" создан\n");
-        } catch (IOException ex) {
-        }
+        Main.createDir("src", str);
+        Main.createDir("res", str);
+        Main.createDir("savegames", str);
+        Main.createDir("temp", str);
+        Main.createDir("src/main", str);
+        Main.createDir("src/test", str);
 
 
-        File drawables = new File("/Users/olakulikova/Games/res/drawables");
-        drawables.mkdir();
-        str.append("Каталог ").append(drawables.getName()).append(" создан\n");
-        File vectors = new File("/Users/olakulikova/Games/res/vectors");
-        vectors.mkdir();
-        str.append("Каталог ").append(vectors.getName()).append(" создан\n");
-        File icons = new File("/Users/olakulikova/Games/res/icons");
-        icons.mkdir();
-        str.append("Каталог ").append(icons.getName()).append(" создан\n");
+        Main.createFile("src/main//Main.java", str);
+        Main.createFile("src/main//Utils.java", str);
 
-        File tempTxt = new File("/Users/olakulikova/Games/temp//temp.txt");
-        try {
-            tempTxt.createNewFile();
-            str.append("Файл ").append(tempTxt.getName()).append(" создан\n");
-        } catch (IOException ex) {
-        }
-        try (FileWriter fileWriter = new FileWriter(tempTxt, false)) {
+
+        Main.createDir("res/drawables", str);
+        Main.createDir("res/vectors", str);
+        Main.createDir("res/icons", str);
+
+
+        Main.createFile("temp//temp.txt", str);
+
+        try (FileWriter fileWriter = new FileWriter("/Users/olakulikova/Games/temp//temp.txt", false)) {
             fileWriter.write(String.valueOf(str));
-        } catch (IOException ex) {
+        } catch (IOException e) {
+            System.out.println("Не получилось записать данные в файл " + e.getMessage());
         }
 
 
+    }
+
+    static private void createDir(String dirName, StringBuilder str) {
+        File dir = new File("/Users/olakulikova/Games/" + dirName);
+        dir.mkdir();
+        str.append("Каталог ").append(dir.getName()).append(" создан\n");
+    }
+
+    static private void createFile(String fileName, StringBuilder str) {
+        File file = new File("/Users/olakulikova/Games/" + fileName);
+        try {
+            file.createNewFile();
+            str.append("Файл ").append(file.getName()).append(" создан\n");
+        } catch (IOException e) {
+            System.out.println("Файл " + file.getName() + " не создан " + e.getMessage());
+        }
     }
 }
